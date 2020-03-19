e35bd78c204da6349840ecbd768c96b682e33ce7
Knut Petter Meen <kp@scalytica.net>
2020-03-19 12:43:44 +0100

    Adding cleanKafka task to the DockerTasksPlugin [skip ci]

--- project/DockerTasksPlugin.scala
+++ project/DockerTasksPlugin.scala
@@ -11,16 +11,25 @@ object DockerTasksPlugin extends AutoPlugin {
     lazy val stopKafka    = taskKey[Unit]("Stop single-node cluster.")
     lazy val restartKafka = taskKey[Unit]("Restart single-node cluster.")
     lazy val statusKafka  = taskKey[Unit]("Check single-node cluster status.")
+    lazy val cleanKafka   = settingKey[Boolean]("If true, will pass -c to init")
 
   }
 
   import autoImport._
 
-  override def globalSettings: Seq[Setting[_]] = Seq(
-    startKafka := { "./docker/single-node/init.sh start" ! },
-    stopKafka := { "./docker/single-node/init.sh stop" ! },
-    restartKafka := { "./docker/single-node/init.sh restart" ! },
-    statusKafka := { "./docker/single-node/init.sh status" ! }
-  )
+  override def globalSettings: Seq[Setting[_]] = {
+    def initScript(cmd: String, clean: Boolean) = {
+      val args = if (clean) " -c" else ""
+      s"./docker/single-node/init.sh $cmd$args"
+    }
+
+    Seq(
+      cleanKafka := true,
+      startKafka := { initScript("start", cleanKafka.value) ! },
+      stopKafka := { initScript("stop", cleanKafka.value) ! },
+      restartKafka := { initScript("restart", cleanKafka.value) ! },
+      statusKafka := { initScript("status", cleanKafka.value) ! }
+    )
+  }
 
 }
