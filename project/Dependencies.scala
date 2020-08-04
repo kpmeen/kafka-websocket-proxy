888c7413f96e5b6e10501926a4354d57024c9ee0
Knut Petter Meen <kp@scalytica.net>
2020-08-04 23:05:00 +0000

    Update akka-actor, ... to 2.6.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Versions {
   val EmbeddedSchemaRegistryVersion = "5.5.0.1"
   val KafkaStreamsQueryVersion = "0.1.1"
 
-  val AkkaVersion = "2.6.6"
+  val AkkaVersion = "2.6.8"
   val AkkaHttpVersion = "10.1.12"
   val AkkaStreamKafkaVersion = "2.0.3"
 
