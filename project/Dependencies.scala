62db17c26416e2ef9d16382a0dd97947f72043eb
Scala Steward <security@scalytica.net>
2021-05-18 01:08:32 +0000

    Update avro4s-core, avro4s-kafka to 4.0.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Versions {
   val LightbendConfigVersion = "1.4.1"
   val PureConfigVersion      = "0.15.0"
 
-  val Avro4sVersion                 = "4.0.7"
+  val Avro4sVersion                 = "4.0.9"
   val ConfluentPlatformVersion      = "6.1.0"
   val KafkaVersion                  = s"$ConfluentPlatformVersion-ccs"
   val EmbeddedSchemaRegistryVersion = ConfluentPlatformVersion
