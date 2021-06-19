fef3b8471b951db535368023ca156226a9a04ed9
Scala Steward <security@scalytica.net>
2021-06-19 01:08:11 +0000

    Update jul-to-slf4j, log4j-over-slf4j, ... to 1.7.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Versions {
   val JwtScalaVersion = "8.0.2"
 
   val ScalaLoggingVersion = "3.9.3"
-  val Slf4JVersion        = "1.7.30"
+  val Slf4JVersion        = "1.7.31"
   val LogbackVersion      = "1.2.3"
   val LogbackJsVersion    = "0.1.5"
   val JaninoVersion       = "3.0.6"
