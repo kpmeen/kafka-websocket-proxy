e839f67b1b7c518f84e1cd1e0bd3c1ef6943cf12
Scala Steward <security@scalytica.net>
2022-09-07 01:02:21 +0000

    Update akka-actor, ... to 2.6.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val EmbeddedKafkaVersion          = ApacheKafkaVersion
   val EmbeddedSchemaRegistryVersion = s"$ConfluentPlatformBaseVersion.+"
 
-  val AkkaVersion            = "2.6.19"
+  val AkkaVersion            = "2.6.20"
   val AkkaHttpVersion        = "10.2.9"
   val AkkaStreamKafkaVersion = "3.0.0"
 
