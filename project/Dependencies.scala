733afbf2e0c22614a5d1e3b43546621cacbffd38
Knut Petter Meen <kp@scalytica.net>
2024-05-03 07:31:36 +0000

    Update jul-to-slf4j, log4j-over-slf4j, ... to 2.0.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Versions {
 
   // logging
   val ScalaLoggingVersion = "3.9.5"
-  val Slf4JVersion        = "2.0.7"
+  val Slf4JVersion        = "2.0.13"
   val LogbackVersion      = "1.4.14"
   val LogbackJsVersion    = "0.1.5"
   val JaninoVersion       = "3.1.9"
